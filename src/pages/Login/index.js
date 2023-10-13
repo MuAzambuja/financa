6bfc57d7daaac22f0189bf15725b72813eb14343
Murilo Azambuja <mu.azambuja@gmail.com>
2023-10-13 14:08:55 -0300

    Design login finalizado

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,31 +1,57 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity, Platform, Link } from 'react-native';
+import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity, Platform, KeyboardAvoidingView, Keyboard, TouchableWithoutFeedback } from 'react-native';
 import { Rotas } from '../Rotas/index'
+import { FontAwesome5 } from '@expo/vector-icons'
 
 export default function Login() {
     return (
-        <View style={styles.container}>
-            <Image
-                source={require('../../images/wallet.png')}
-                style={styles.logo}
-            />
-            <TextInput
-                placeholder='Username ou e-mail'
-                style={styles.input}
-            />
-            <TextInput
-                placeholder='Senha'
-                style={styles.input}
-            />
-            <View style={styles.forgotContainer}>
-                <TouchableOpacity>
-                    <Text style={styles.forgotText}>Esqueceu sua senha?</Text>
-                </TouchableOpacity>
-            </View>
-            <TouchableOpacity style={styles.loginButton}>
-                <Text style={styles.loginText}>Acessar</Text>
-            </TouchableOpacity>
-        </View>
+        <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        >
+            <TouchableWithoutFeedback
+                onPress={Keyboard.dismiss}
+            >
+                <View style={styles.container}>
+                    <Image
+                        source={require('../../images/wallet.png')}
+                        style={styles.logo}
+                    />
+                    <TextInput
+                        placeholder='Username ou e-mail'
+                        style={styles.input}
+                    />
+                    <TextInput
+                        placeholder='Senha'
+                        style={styles.input}
+                    />
+                    <View style={styles.forgotContainer}>
+                        <TouchableOpacity>
+                            <Text style={styles.forgotText}>Esqueceu sua senha?</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <TouchableOpacity style={styles.loginButton}>
+                        <Text style={styles.loginText}>Acessar</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.gmailButton}>
+                        <FontAwesome5 name='google' size={25} color='#0257be' />
+                        <Text style={styles.gmailText}>Continue como MaZaM</Text>
+                    </TouchableOpacity>
+
+                    <View style={styles.divisor}>
+                        <View style={styles.divisorLine}></View>
+                        <Text style={{ marginHorizontal: '3%' }}>OU</Text>
+                        <View style={styles.divisorLine}></View>
+                    </View>
+                    <View style={styles.signUpContainer}>
+                        <Text style={styles.signUpText}>Não possui uma conta? </Text>
+                        <TouchableOpacity>
+                            <Text style={styles.signUpButton}>Cadastre-se</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
     );
 }
 
@@ -36,31 +62,79 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     logo: {
-        marginTop: Platform.OS === 'android' ? '13%' : '20%',
-
+        height: 350,
+        marginTop: Platform.OS === 'android' ? '8%' : '10%',
+        marginBottom: Platform.OS === 'android' ? '1%' : '5%',
     },
     input: {
         width: '90%',
         height: 42,
         backgroundColor: '#F4F3F3',
-
+        marginBottom: 20,
+        padding: 8,
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#E0E0E0',
     },
     forgotContainer: {
-
+        width: '90%',
+        paddingBottom: 5,
+        paddingTop: 5,
+        alignItems: 'flex-end',
     },
     forgotText: {
-
+        color: '#0257be',
     },
     loginButton: {
+        marginTop: '5%',
         backgroundColor: '#1C02BA',
-        padding: 10,
-        borderRadius: 8,
-        alignItems: 'center',
+        width: '90%',
+        height: 42,
         justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 5,
     },
     loginText: {
         color: '#fff',
+        fontSize: 16,
         fontWeight: 'bold',
         textAlign: 'center',
+    },
+    gmailButton: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: '8%',
+    },
+    gmailText: {
+        color: '#0257be',
+        paddingLeft: 8,
+        fontSize: 15,
+    },
+    divisor: {
+        marginTop: '8%',
+        flexDirection: 'row',
+        width: '90%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    divisorLine: {
+        width: '45%',
+        height: 2,
+        backgroundColor: '#EFEDED',
+        borderRadius: 5,
+    },
+    signUpContainer: {
+        flexDirection: 'row',
+        marginTop: '8%',
+    },
+    signUpText: {
+        fontSize: 15,
+        color: '#C4C4C4',
+    },
+    signUpButton: {
+        color: '#0257be',
+        paddingLeft: 2,
+        fontSize: 15,
+        fontWeight: 'bold',
     },
 })
